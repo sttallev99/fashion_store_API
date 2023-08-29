f4000793a835cdddac1cffb7927d2627a9a2b3fe
Георги Сталев <sttallev99@gmail.com>
2023-08-29 13:38:11 +0300

    create routes files and implement register route

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
-app.use('/api/user', userRoute);
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('DB CONNECTED'))

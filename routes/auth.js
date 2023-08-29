cf1ae1862edd86de4e8e398547c664073210e8c4
Георги Сталев <sttallev99@gmail.com>
2023-08-29 15:06:10 +0300

    add jwt token to login response

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -30,9 +31,15 @@ router.post('/login', async (req, res) => {
         const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
         Originalpassword !== req.body.password && res.status(401).json('Wrong credentionals!');
 
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_SEC,
+        {expiresIn: '3d'});
+
         const {password, ...others} = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     }catch(err) {
         res.status(500).json(err);
     }

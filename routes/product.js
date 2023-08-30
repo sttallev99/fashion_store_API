e8b22993bfcad47f764113996b2239e19f3a509a
Георги Сталев <sttallev99@gmail.com>
2023-08-30 20:08:30 +0300

    Create delete get and get all products

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { verifyTokenAndAdmin } = require('./verifyToken');
+const { verifyTokenAndAdmin, verifyTokenAndAuthorization } = require('./verifyToken');
 const Product = require('../models/Product');
 
 //CREATE PRODUCT
@@ -23,5 +23,52 @@ router.put('/:id', verifyTokenAndAdmin, async(req, res) => {
         res.status(500).json(err);
     } 
 });
+
+//DELETE PRODUCT
+router.delete('/:id', verifyTokenAndAdmin, async(req, res) => {
+    try{
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json('Product has beed deleted successfully!');
+    }catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET PRODUCT
+router.get('/find/:id', async(req, res) => {
+    try{
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET ALL PRODUCTS
+router.get('/', async(req, res) => {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
+    try{
+        let products;
+
+        if(qNew) {
+            products = await Product.find().sort({createdAt: -1}).limit(5);
+        } else if(qCategory) {
+            products = await Product.find({
+                categories: {
+                    $in: [qCategory]
+                }
+            })
+        } else {
+            products = await Product.find();
+        }
+
+        res.status(200).json(products);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+
 
 module.exports = router;

1a735d7ac80c4e70039a024c8ff48300b29e165e
Георги Сталев <sttallev99@gmail.com>
2023-09-04 15:01:19 +0300

    Implement CRUD operation for orders routes

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ require('dotenv').config();
 
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
-const productRoute = require('./routes/product');
+const productsRoute = require('./routes/product');
+const cartsRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
-app.use('/api/products', productRoute);
+app.use('/api/products', productsRoute);
+app.use('/api/carts', cartsRoute);
+app.use('/api/orders', orderRoute);
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('DB CONNECTED'))

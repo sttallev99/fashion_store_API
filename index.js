d06fc7a0885d6ac528e61280095b6f6578862847
Георги Сталев <sttallev99@gmail.com>
2023-09-05 17:18:13 +0300

    redesign Product model and add stripe route

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productsRoute = require('./routes/product');
 const cartsRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoute);
@@ -16,6 +19,7 @@ app.use('/api/auth', authRoute);
 app.use('/api/products', productsRoute);
 app.use('/api/carts', cartsRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute)
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('DB CONNECTED'))
--- models/Product.js
+++ models/Product.js
@@ -18,16 +18,20 @@ const ProductSchema = new mongoose.Schema({
         type: Array
     },
     size: {
-        type: String,
+        type: Array,
         required: true
     },
     color: {
-        type: String,
+        type: Array,
         reuired: true
     },
     price: {
         type: Number,
         required: true
+    },
+    inStock: { 
+        type: Boolean, 
+        default: true
     }
 }, {timestamps: true});
 
